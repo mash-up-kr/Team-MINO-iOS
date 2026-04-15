d4aaaf8570fc47ca328a70f9df8cd4077f9df9fb
hooni <jhlee909090@gmail.com>
2026-04-15 23:01:19 +0900

    feat(app): Add core application boilerplate files

---
+++ Targets/iOS-mini-fig/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-mini-fig
+//
+//  Created by 이지훈 on 4/15/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Home"
+    }
+
+
+}
+
