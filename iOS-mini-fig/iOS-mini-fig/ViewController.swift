f05075314b7e8c68c1e72fa9477da6cd3791d9db
hooni <jhlee909090@gmail.com>
2026-04-15 22:50:17 +0900

    기본 XCODE init

---
+++ iOS-mini-fig/iOS-mini-fig/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-mini-fig
+//
+//  Created by 이지훈 on 4/15/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
