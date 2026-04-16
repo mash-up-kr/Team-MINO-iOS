79ffb00a23757f14b4dc186036b592c97d00be7e
YIZIHN <jhlee909090@gmail.com>
2026-04-16 22:53:52 +0900

    코드리뷰봇을 테스트해봐요 (#4)
    * test
    
    ㅅ
    
    * role 수정

---
+++ Targets/iOS-mini-fig/Sources/testViewController.swift
@@ -0,0 +1,13 @@
+//
+//  testViewController.swift
+//  iOS-mini-fig
+//
+//  Created by 이지훈 on 4/16/26.
+//  Copyright © 2026 yizihn. All rights reserved.
+//
+
+import Foundation
+
+/// -듀아아아
+/// 아아아아
+/// 으아아아
