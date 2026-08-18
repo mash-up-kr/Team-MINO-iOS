4c6db6be4989c569b75e014ac158ce08b38dd898
YIZIHN <jhlee909090@gmail.com>
2026-08-18 23:24:35 +0900

    build(Deps): Domain 패키지에 macOS 플랫폼 추가

--- Packages/Domain/Package.swift
+++ Packages/Domain/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Domain",
-    platforms: [.iOS(.v17)],
+    platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
         .library(name: "Domain", targets: ["Domain"]),
     ],
